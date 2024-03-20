7202af761fa6ffe484bae2f5d29ffd59e9277d67
adryanbartolome <adryanbartolome8123@gmail.com>
2024-03-19 21:29:32 -0700

    I added my basic JavaScrip Message to my doc. More will be added once I learn more JavaScrip!

---
+++ index.js
@@ -0,0 +1,5 @@
+const message1 = "This is the basic JavaScript that I have included for my website.";
+
+const message2 = "I do not know much JavaScript yet but I will add more to this section later on"
+
+console.log(`${message1} ${message2}`)
